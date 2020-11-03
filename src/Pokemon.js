29befeab5b3b2dbc16b5ce7762838ab706634afc
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-11-03 22:51:57 +0900

    add prev and next button to pokemon page

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Link, Button, CircularProgress } from '@material-ui/core';
+import {
+	Typography,
+	Link,
+	Button,
+	ButtonGroup,
+	CircularProgress,
+	Container
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { toFirstCharUppercase } from './constants';
 import axios from 'axios';
+
+const useStyles = makeStyles(() => ({
+	pokemonContainer: {
+		textAlign: 'center'
+	}
+}))
 
 const Pokemon = (props) => {
 	const { history } = props;
 	const pokemonId = props.match.params.id;
 	const [pokemon, setPokemon] = useState(undefined);
+	const classes = useStyles();
 
 	useEffect(() => {
 		axios
@@ -26,12 +41,16 @@ const Pokemon = (props) => {
 		const { front_default } = sprites;
 
 		return (
-			<>
+			<Container className={classes.pokemonContainer}>
 				<Typography variant="h1">
 					{`${id}.`} {toFirstCharUppercase(name)}
-					<img src={front_default} alt={name}/>
+					<img src={front_default} alt={name} />
 				</Typography>
-				<img style={{ width: '300px', height: '300px' }} src={fullImageUrl} alt={name}/>
+				<img
+					style={{ width: '300px', height: '300px' }}
+					src={fullImageUrl}
+					alt={name}
+				/>
 				<Typography variant="h3">Pokemon Info</Typography>
 				<Typography>
 					{'Species: '}
@@ -44,7 +63,24 @@ const Pokemon = (props) => {
 					const { name } = type.type;
 					return <Typography key={name}>{`${name}`}</Typography>;
 				})}
-			</>
+				<ButtonGroup>
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={() => history.push(`/${+pokemonId - 1}`)}
+					>
+						Previous
+					</Button>
+					<Button
+						variant="contained"
+						color="secondary"
+						onClick={() => history.push(`/${+pokemonId + 1}`)}
+					>
+						Next
+					</Button>
+				</ButtonGroup>
+				<br />
+			</Container>
 		);
 	};
 
@@ -54,7 +90,15 @@ const Pokemon = (props) => {
 			{pokemon !== undefined && pokemon && generatePokemonJSX()}
 			{pokemon === false && <Typography>Pokemon not found</Typography>}
 			{pokemon !== undefined && (
-				<Button variant="contained" color="primary" onClick={() => history.push("/")}> Back to Pokedex</Button>
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={() => history.push('/')}
+					fullWidth
+				>
+					{' '}
+					Back to Pokedex
+				</Button>
 			)}
 		</>
 	);

be42d67216cbb9cdfe222d1e1d386493c7cb88d2
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-11-06 10:52:48 +0900

    update router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Pokedex from './Pokedex';
 import Pokemon from './Pokemon';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter basename="/">
 			<Switch>
 				<Route exact path="/" render={(props) => <Pokedex {...props} />} />
 				<Route exact path="/:id" render={(props) => <Pokemon {...props} />} />
 			</Switch>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 

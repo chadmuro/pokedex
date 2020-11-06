f4172aaa83cac48591237dd3fb40c08b84f90896
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-11-06 10:45:57 +0900

    update router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Pokedex from './Pokedex';
 import Pokemon from './Pokemon';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<Switch>
-			<Route exact path="/pokedex" render={(props) => <Pokedex {...props} />} />
-			<Route exact path="/pokedex/:id" render={(props) => <Pokemon {...props} />} />
-		</Switch>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Switch>
+				<Route exact path="/" render={(props) => <Pokedex {...props} />} />
+				<Route exact path="/:id" render={(props) => <Pokemon {...props} />} />
+			</Switch>
+		</BrowserRouter>
 	);
 };
 
